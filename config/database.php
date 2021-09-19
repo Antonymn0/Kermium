bf7999370f52bbd64e42e9e9ffd3d6f0367135f3
antomymn <mnantony0@gmail.com>
2021-09-19 10:45:56 +0300

    update db connection for heroku

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql') ? env('DB_CONNECTION') : 'pgsql',
+    'default' => 'pgsql',
 
     /*
     |--------------------------------------------------------------------------
